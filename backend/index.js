9ebe17056bdd684ca556713d7d5bf5aab3c04776
Mohd. Shayan <70843731+MohdShayan@users.noreply.github.com>
2024-10-03 22:54:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({ origin: 'http://localhost:5173' }));
-const PORT=process.env.PORT || 5000
+const PORT=process.env.PORT || 3000
 
 app.use(express.json());
 
