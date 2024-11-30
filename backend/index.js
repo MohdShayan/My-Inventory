726500cb6094442f468feca4d039c9d1dd178c43
Mohd. Shayan <70843731+MohdShayan@users.noreply.github.com>
2024-11-30 09:34:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import productRoutes from "./routes/productRoutes.js"
 dotenv.config();
 const app = express();
 
-ConnectDB(),then(()=>{
+ConnectDB().then(()=>{
 
 app.use(cors());
 const PORT=process.env.PORT || 3000
